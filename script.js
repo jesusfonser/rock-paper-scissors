95568f790346b91d16250045098f7d700170e785
jesusfonser <jesusfonseruma@gmail.com>
2024-07-12 17:10:22 +0200

    Crea pseudocódigo

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+
+Crea función getComputerChoice
+    Haz que devuelva uno de estos string de manera aleatoria: rock paper scissors
+    Haz console.log al valor devuelto por getComputerChoice
+
+Crea función getHumanChoice
+    Pide input al usuario con prompt
+    Función devuelve rock, paper o scissors dependiendo de lo que introduzca el usuario
+    Haz console.log al valor devuelto por la función
+
+Crea variable humanScore. Inicialízala con valor 0
+Crea variable computerScore. Inicialízala con valor 0
+
+Crea función playRound. Sus parámetros son humanChoice y computerChoice
+    Parámetro humanChoice debe ser case-insensitive
+    Función hará console.log para anunciar al ganador de la ronda así:
+        "You win/lose! (elección ganadora) beats (elección perdedora)"
+    Aumenta en 1 humanScore o computerScore dependiendo de quién ganó la ronda
+
+Crea función playGame
+    Invoca playRound 5 veces
+    Muestra puntuación de ordenador y jugador
+    Una vez se haya hecho playRound 5 veces, declara ganador
+
+/*
