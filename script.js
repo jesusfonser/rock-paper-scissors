c4690e705ca31b7e2407525361df442f115d159a
jesusfonser <jesusfonseruma@gmail.com>
2024-07-18 17:24:30 +0200

    Convierte el input del user en minúsculas

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function getComputerChoice(){
 console.log(getComputerChoice())
 
 function getHumanChoice(){
-    let userDecision = prompt("Escribe 'rock', 'paper' o 'scissors'")
+    let userDecision = prompt("Escribe 'rock', 'paper' o 'scissors'").toLowerCase()
     switch(userDecision){
         case "rock":
             return "rock"
@@ -74,3 +74,35 @@ console.log(getHumanChoice())
 
 let humanScore = 0
 let computerScore = 0
+
+
+
+
+/*
+PASO 4:
+
+Crea función playRound. Sus parámetros son humanChoice y computerChoice
+    Función hará console.log para anunciar al ganador de la ronda así:
+        "You win/lose! (elección ganadora) beats (elección perdedora)"
+    Aumenta en 1 humanScore o computerScore dependiendo de quién ganó la ronda
+
+    Función playRound, parámetros humanChoice y computerChoice
+
+        if humanChoice es igual a computerChoice
+            console.log ¡Empate! Se repite la ronda.
+
+        else if humanChoice es scissors y computerChoice es rock
+            console.log ¡Has perdido! Rock le gana a Scissors
+        else if humanChoice es scissors y computerChoice es paper
+            console.log ¡Has ganado! Scissors le gana a Paper
+
+        else if humanChoice es paper y computerChoice es scissors
+            console.log ¡Has perdido! Scissors le gana a Paper
+        else if humanChoice es paper y computerChoice es rock
+            console.log ¡Has ganado! Paper le gana a Rock
+
+        else if humanChoice es rock y computerChoice es paper
+            console.log ¡Has perdido! Paper le gana a Rock
+        else if humanChoice es rock y computerChoice es scissors
+            console.log ¡Has ganado! Rock le gana a Scissors
+    */
